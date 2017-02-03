d771e29b75c391c6d14c3b4483fc752f50fec8db
GlaIZier <khokh.mv@gmail.com>
2017-02-03 19:39:41 +0300

    Main class has been edited

--- src/main/java/ru/glaizier/Main.java
+++ src/main/java/ru/glaizier/Main.java
@@ -1,10 +1,12 @@
 package ru.glaizier;
 
+import org.apache.commons.cli.Options;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        Options options = new Options();
 
     }
 
